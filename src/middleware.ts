2b5e536f6bc275b3660d775db92adc0c5365b533
John <johnnyz88@gmail.com>
2023-03-14 17:42:03 +1100

    comments

--- src/middleware.ts
+++ src/middleware.ts
@@ -11,8 +11,11 @@ export async function middleware(nextRequest: NextRequest) {
 	const pathname = nextRequest.nextUrl.pathname;
 
 	if (matcher(pathname, "/some-route/:id*")) {
+		// Needs to be called inside route as it consumes the request.
+		// Once consumed the request body cannot be used by code that runs after the middleware, e.g. API routes.
 		const request = new MiddlewareRequest(nextRequest);
 		const response = await request.next();
+		//
 		console.log("Matched /some-route/:id*");
 		return response;
 	}
